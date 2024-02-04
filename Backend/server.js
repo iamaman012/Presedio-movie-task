bf7cbf27664b5aca8c3c192c68352c974c3e6749
iamaman012 <111675545+iamaman012@users.noreply.github.com>
2024-02-04 18:07:56 +0530

    PATH addes

--- Backend/server.js
+++ Backend/server.js
@@ -3,11 +3,21 @@ import express from "express";
 import router from "./routes/routes.js";
 import { loadMoviesFromFile } from "./controller/controller.js";
 import cors from "cors";
+import path from 'path';
 // import dotenv from "dotenv";
 const app = express();
 
 // dotenv.config();
 app.use(cors());
+const buildPath = path.join(__dirname, 'build');
+
+// Serve static files from the build folder
+app.use(express.static(buildPath));
+
+// Serve the index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'));
+});
 
 // app.get("/", (req, res) => {
 //   res.send("<h1> Welcome to Movie App");
