4b5f2082f4870de869fdf3887a7bc6080b1c277e
iamaman012 <111675545+iamaman012@users.noreply.github.com>
2024-02-04 18:22:34 +0530

    path added

--- Backend/server.js
+++ Backend/server.js
@@ -3,12 +3,19 @@ import express from "express";
 import router from "./routes/routes.js";
 import { loadMoviesFromFile } from "./controller/controller.js";
 import cors from "cors";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import path from 'path';
 // import dotenv from "dotenv";
 const app = express();
 
 // dotenv.config();
 app.use(cors());
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Define the path to your build folder
+// console.log(__dirname)
 const buildPath = path.join(__dirname, 'build');
 
 // Serve static files from the build folder
